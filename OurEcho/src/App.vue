c0148ced4a9aaf688dcc17fe7fb8fc4ac906b9f4
Tin Baltazar <57336759+kmrbaltazar@users.noreply.github.com>
2023-02-23 09:20:37 +1300

    Added news categories array on main

--- OurEcho/src/App.vue
+++ OurEcho/src/App.vue
@@ -1,15 +1,16 @@
 <script setup>
-import Header from './components/home_components/Header.vue'
-import SearchBar from './components/home_components/SearchBar.vue'
+import Header from "./components/home_components/Header.vue";
+import SearchBar from "./components/home_components/SearchBar.vue";
 </script>
 
 <template>
-<Header />
-<main>
-<h1>Get access to top news globally</h1>
-<SearchBar />
-</main>
-
+  <Header />
+  <main>
+    <section class="home_section">
+      <h1>Get access to top news globally</h1>
+      <SearchBar />
+    </section>
+  </main>
 </template>
 
 <style scoped>
@@ -21,15 +22,16 @@ h1 {
 main {
   margin: 20px;
 }
-
 </style>
 
 <script>
 export default {
-  data(){ // return part of data() is where we declare variables
-    return{ // declare variables below and separate them with comma ,
-
-    } // end of variable declaration area
-  }
-}
+  data() {
+    // return part of data() is where we declare variables
+    return {
+      // declare variables below and separate them with comma ,
+      news_top_categories: ['Government','Politics','Economy','Environment','Business','Technology','Fashion','Sports','Entertainment']
+    }; // end of variable declaration area
+  },
+};
 </script>
